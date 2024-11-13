46155c074e95bc80dc11dc3dca7616f7fa3edd89
Maurycy Szmuc <mauryc_s@wp.pl>
2024-11-13 19:34:18 +0100

    Initial commit, added main script

---
+++ main.py
@@ -0,0 +1,12 @@
+import openai
+from openai import OpenAI,AsyncOpenAI
+import asyncio
+import requests
+
+def main():
+    with open("artykuł.txt") as article:
+        pass
+    pass
+
+if __name__ == "__main__":
+    main()
